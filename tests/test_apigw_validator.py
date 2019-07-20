32c820fcab972d7a7b874811856ec46c68f19cde
sunwei <me@sunwei.xyz>
2019-07-20 08:19:06 +0800

    Modified: check proxy; Wayde

--- tests/test_apigw_validator.py
+++ tests/test_apigw_validator.py
@@ -16,18 +16,34 @@ def test_v1_check_metadata_with_right_format(api_gw_json_data):
     assert result is True
 
 
-@pytest.mark.usefixtures("api_gw_json_data")
-def test_v1_check_route_specification_with_right_format(api_gw_json_data):
-    api_gw_validator_instance = create_api_gw_validator("v1")
-    result = api_gw_validator_instance.check_route_specification(api_gw_json_data["routeSpecification"])
-    assert result is True
-
-
 @pytest.mark.usefixtures("api_gw_wrong_json_data")
 def test_v1_check_metadata_with_wrong_format(api_gw_wrong_json_data):
     api_gw_validator_instance = create_api_gw_validator("v1")
     result = api_gw_validator_instance.check_metadata(api_gw_wrong_json_data["metadata"])
     assert result is False
+
+
+@pytest.mark.usefixtures("api_gw_json_data")
+def test_v1_check_proxy_with_right_format(api_gw_json_data):
+    api_gw_validator_instance = create_api_gw_validator("v1")
+    proxy = api_gw_json_data["routeSpecification"][0]
+    result = api_gw_validator_instance.check_proxy(proxy)
+    assert result is True
+
+
+@pytest.mark.usefixtures("api_gw_wrong_json_data")
+def test_v1_check_proxy_with_wrong_format(api_gw_wrong_json_data):
+    api_gw_validator_instance = create_api_gw_validator("v1")
+    proxy = api_gw_wrong_json_data["routeSpecification"][0]
+    result = api_gw_validator_instance.check_proxy(proxy)
+    assert result is False
+
+
+@pytest.mark.usefixtures("api_gw_json_data")
+def test_v1_check_route_specification_with_right_format(api_gw_json_data):
+    api_gw_validator_instance = create_api_gw_validator("v1")
+    result = api_gw_validator_instance.check_route_specification(api_gw_json_data["routeSpecification"])
+    assert result is True
 
 
 @pytest.mark.usefixtures("api_gw_wrong_json_data")
