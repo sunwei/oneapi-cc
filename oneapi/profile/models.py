9245dee39e7efae8ca2c1954e4229e4f87ced120
sunzhongmou <sunzhongmou820@163.com>
2019-08-08 11:02:03 +0800

    Modified: add migration; Wayde

--- oneapi/profile/models.py
+++ oneapi/profile/models.py
@@ -1,11 +1,4 @@
-from flask_jwt_extended import current_user
-
 from oneapi.database import (Model, Column, SurrogatePK, db, reference_col, relationship)
-
-
-followers_assoc = db.Table("followers_assoc",
-                           db.Column("follower", db.Integer, db.ForeignKey("userprofile.user_id")),
-                           db.Column("followed_by", db.Integer, db.ForeignKey("userprofile.user_id")))
 
 
 class UserProfile(Model, SurrogatePK):
@@ -16,38 +9,11 @@ class UserProfile(Model, SurrogatePK):
 
     user_id = reference_col('users', nullable=False)
     user = relationship('User', backref=db.backref('profile', uselist=False))
-    follows = relationship('UserProfile',
-                           secondary=followers_assoc,
-                           primaryjoin=id == followers_assoc.c.follower,
-                           secondaryjoin=id == followers_assoc.c.followed_by,
-                           backref='followed_by',
-                           lazy='dynamic')
     bio = Column(db.String(300), nullable=True)
     image = Column(db.String(120), nullable=True)
 
     def __init__(self, user, **kwargs):
         db.Model.__init__(self, user=user, **kwargs)
-
-    def is_following(self, profile):
-        return bool(self.follows.filter(followers_assoc.c.followed_by == profile.id).count())
-
-    def follow(self, profile):
-        if self is not profile and not self.is_following(profile):
-            self.follows.append(profile)
-            return True
-        return False
-
-    def unfollow(self, profile):
-        if self is not profile and self.is_following(profile):
-            self.follows.remove(profile)
-            return True
-        return False
-
-    @property
-    def following(self):
-        if current_user:
-            return current_user.profile.is_following(self)
-        return False
 
     @property
     def username(self):
--- tests/test_profile.py
+++ tests/test_profile.py
@@ -15,31 +15,8 @@ def _register_user(testapp, **kwargs):
 
 class TestProfile:
 
-    def test_get_profile_not_loggedin(self, testapp):
-        _register_user(testapp)
-        resp = testapp.get(url_for('profiles.get_profile', username='foobar'))
-        assert resp.json['profile']['email'] == 'foo@bar.com'
-        assert not resp.json['profile']['following']
-
     def test_get_profile_not_existing(self, testapp):
         resp = testapp.get(url_for('profiles.get_profile', username='foobar'), expect_errors=True)
         assert resp.status_int == 404
         assert resp.json == USER_NOT_FOUND['message']
 
-    def test_follow_user(self, testapp, user):
-        user = user.get()
-        resp = _register_user(testapp)
-        token = str(resp.json['user']['token'])
-        resp = testapp.post(url_for('profiles.follow_user', username=user.username), headers={
-            'Authorization': 'Token {}'.format(token)
-        })
-        assert resp.json['profile']['following']
-
-    def test_unfollow_user(self, testapp, user):
-        user = user.get()
-        resp = _register_user(testapp)
-        token = str(resp.json['user']['token'])
-        resp = testapp.delete(url_for('profiles.unfollow_user', username=user.username), headers={
-            'Authorization': 'Token {}'.format(token)
-        })
-        assert not resp.json['profile']['following']
